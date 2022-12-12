7cce54956ef40e0195501d4da958309303dca1e7
Kat <cat.prokhorova@gmail.com>
2022-12-12 16:20:12 +0600

    Shiet Done

--- thy_thing.py
+++ thy_thing.py
@@ -1,7 +1,8 @@
 import requests
 import pandas as pd
+import re
 from tqdm import tqdm
-import time
+from time import sleep
 import os
 from pprint import pprint
 import json
@@ -18,15 +19,21 @@ resp = requests.get(url, headers=headers).text
 soup = BeautifulSoup(resp, features="html.parser")
 last_page = int(soup.find_all(class_='pager-item-not-in-short-range')[-1].find('span').text)
 
-for i in range(last_page + 1):
-    url = f'{url}&page={i}'
-    resp = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(resp, features="html.parser")
-    cards = soup.find_all(class_='serp-item')
-    for card in cards:
-        title = card.find(class_='serp-item__title').text
-        emp = card.find(class_='bloko-v-spacing-container bloko-v-spacing-container_base-2').text
-        city = card.find(class_='vacancy-serp__vacancy-address').text
-        print(title, city)
-        break
-    break
+def get_id(url, pages):
+    count = 0
+    for i in range(pages + 1):
+        url = f'{url}&page={i}'
+        resp = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(resp, features="html.parser")
+        cards = soup.find_all(class_='serp-item')
+        sleep(2)
+        for card in cards:
+            count += 1
+            link = card.find('a', class_='serp-item__title')
+            print(count, link.get('href'))
+            id = re.findall(r'[0-9]+', link.get('href'))[0]
+            with open('ids.txt', 'a', encoding='utf-8') as w, open('log.txt', 'a', encoding='utf-8') as l:
+                w.write(id + '\n')
+                l.write(f'{i} {count} {link}\n')
+            sleep(1)
+get_id(url, last_page)
