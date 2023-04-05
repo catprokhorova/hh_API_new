a9833088a7b5052c5dee67550612a71a3ff05184
Ekaterina Prokhorova <cat.prokhorova@gmail.com>
2023-04-05 21:01:19 +0300

    Recent changes

--- vac_retrieval.py
+++ vac_retrieval.py
@@ -5,10 +5,11 @@ from numpy import NaN
 from tqdm import tqdm
 from converter import ConverterHH
 from datetime import datetime
-from time import sleep
+import time
 from dotenv import load_dotenv
 import os
 
+start = time.time()
 load_dotenv()
 
 vert = ConverterHH()
@@ -37,66 +38,73 @@ res = requests.get(url_ar, headers=headers).json()
 for el in res[0]['areas']:
     areas.append(el['id'])
 
-# retrieving list of professional roles
-professional_role = ['157', '79', '156', '10', '150', '165', '164', '148', '41', '163', '134', '40']
+# # retrieving list of professional roles
+# professional_role = ['157', '79', '156', '10', '150', '165', '164', '148', '41', '163', '134', '40']
 
 URL = 'https://api.hh.ru/vacancies'
 
 for query in tqdm(queries):
     result = pd.DataFrame()
     for area in areas:
-        sleep(5)
-        for role in professional_role:
-            sleep(1)
-            pages = requests.get(
-                    URL,
-                    headers=headers, 
-                    params={'text': query,
-                            # 'search_field': 'name',
-                            'professional_role': role,
-                            'area': area,
-                            'archived': False,
-                            'period': 30}
-                ).json()['pages']
-            for page in tqdm(range(pages)):
-                data = {}
-                resp = requests.get(
-                    URL,
-                    headers=headers,
-                    params={'text': query,
-                            # 'search_field': 'name',
-                            'professional_role': role,
-                            'area': area,
-                            'archived': False,
-                            'page': page,
-                            'period': 30}
-                ).json()['items']
-                for j in resp:
-                    data['id'] = j['id']
-                    data['spec'] = query
-                    data['job_title'] = j['name']
-                    data['city'] = j['area']['name']
-                    data['employer'] = j['employer']['name']
-                    if j['schedule']:
-                        data['schedule'] = j['schedule']['name']
+        time.sleep(1)
+        pages = requests.get(
+                URL,
+                headers=headers, 
+                params={'text': query,
+                        'search_field': 'name',
+                        'area': area,
+                        'archived': False,
+                        'period': 30}
+            ).json()['pages']
+        for page in tqdm(range(pages)):
+            data = {}
+            resp = requests.get(
+                URL,
+                headers=headers,
+                params={'text': query,
+                        'search_field': 'name',
+                        'area': area,
+                        'archived': False,
+                        'page': page,
+                        'period': 30}
+            ).json()['items']
+            for j in resp:
+                data['id'] = j['id']
+                data['spec'] = query
+                data['job_title'] = j['name']
+                data['city'] = j['area']['name']
+                data['employer'] = j['employer']['name']
+                if j['schedule']:
+                    data['schedule'] = j['schedule']['name']
+                else:
+                    data['schedule'] = NaN
+                if j.get('experience'):
+                    data['experience'] = j['experience']['name']
+                else:
+                    data['experience'] = NaN
+                data['published'] = datetime.strptime(j['published_at'].split('T')[0], '%Y-%m-%d')
+                salary = j.get('salary')
+                if salary:
+                    course = vert.get_valute(salary['currency'])
+                    if salary['currency'] != 'RUR' and course:
+                        data['salary_from'] = round(salary['from'] / course) if salary['from'] else None
+                        data['salary_to'] = round(salary['to'] / course) if salary['to'] else None
+                        data['currency'] = 'RUR'
                     else:
-                        data['schedule'] = NaN
-                    data['published'] = datetime.strptime(j['published_at'].split('T')[0], '%Y-%m-%d')
-                    salary = j.get('salary')
-                    if salary:
-                        course = vert.get_valute(salary['currency'])
-                        if salary['currency'] != 'RUR' and course:
-                            data['salary_from'] = round(salary['from'] / course) if salary['from'] else None
-                            data['salary_to'] = round(salary['to'] / course) if salary['to'] else None
-                            data['currency'] = 'RUR'
-                        else:
-                            data['salary_from'] = salary['from']
-                            data['salary_to'] = salary['to']
-                            data['currency'] = salary['currency']
-                    else:
-                        data['salary_from'] = salary
-                        data['salary_to'] = salary
-                        data['currency'] = salary
-                    result = pd.concat([result, pd.DataFrame([data])])
+                        data['salary_from'] = salary['from']
+                        data['salary_to'] = salary['to']
+                        data['currency'] = salary['currency']
+                else:
+                    data['salary_from'] = salary
+                    data['salary_to'] = salary
+                    data['currency'] = salary
+                skill = requests.get(f"{URL}/{j['id']}").json()
+                if skill.get('key_skills'):
+                    data['skills'] = ', '.join([s['name'] for s in skill['key_skills']]).lower()
+                else:
+                    data['skills'] = NaN
+                result = pd.concat([result, pd.DataFrame([data])])
 
     result.to_csv(f'results/{query}_vac.csv', sep=';', index=False)
+
+print(time.time() - start, 'СКА!!!!!')
