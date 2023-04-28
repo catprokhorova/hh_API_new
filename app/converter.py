758176f0793482eca18e9bbf6213c5d045a49232
Evgeny Malikov <mev@offershub.ru>
2023-04-28 19:59:47 +0500

    async

--- app/converter.py
+++ app/converter.py
@@ -1,8 +1,6 @@
 from functools import cached_property
-from pprint import pprint
-
 import requests
-
+from pprint import pprint
 
 class Converter:
     """
@@ -11,27 +9,26 @@ class Converter:
 
     @cached_property
     def exchange_rates(self):
-        rate = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
-        return rate.json()["Valute"]
+        r = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        return r.json()['Valute']
 
     def get_valute(self, currency):
         response = self.exchange_rates
         if response.get(currency):
-            return response[currency]["Value"] / response[currency]["Nominal"]
+            return response[currency]['Value'] / response[currency]['Nominal']
 
 
 class ConverterHH:
     """
     Using HH API
     """
-
     @cached_property
     def exchange_rates(self):
-        r = requests.get("https://api.hh.ru/dictionaries/")
-        return r.json()["currency"]
+        r = requests.get('https://api.hh.ru/dictionaries/')
+        return r.json()['currency']
 
     def get_valute(self, currency):
         response = self.exchange_rates
         for el in response:
-            if currency == el["code"]:
-                return el["rate"]
+            if currency == el['code']:
+                return el['rate']
